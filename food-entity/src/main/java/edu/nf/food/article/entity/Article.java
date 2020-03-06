c2dae56e1a7b76b31c3e4f213bb2eb307fdbe852
ljf <2039557184@qq.com>
2020-03-06 12:03:57 +0800

    测试 包

---
+++ food-entity/src/main/java/edu/nf/food/article/entity/Article.java
@@ -0,0 +1,8 @@
+package edu.nf.food.article.entity;
+
+/**
+ * @author admin
+ * @date 2020/3/6
+ */
+public class Article {
+}
---
+++ food-entity/src/main/java/edu/nf/food/video/entity/Video.java
@@ -0,0 +1,8 @@
+package edu.nf.food.video.entity;
+
+/**
+ * @author admin
+ * @date 2020/3/6
+ */
+public class Video {
+}
