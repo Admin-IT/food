ec89e1c0f0169d7305131d8942c19fae3495ccfc
ljf <2039557184@qq.com>
2020-04-07 13:48:33 +0800

    去除问题

--- food-web/src/main/java/edu/nf/food/util/testController.java
+++ food-web/src/main/java/edu/nf/food/util/testController.java
@@ -1,8 +1,15 @@
 package edu.nf.food.util;
 
+import edu.nf.food.vo.ResponseVO;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author ljf
  * @date 2020/4/4
  */
 public class testController {
+    @GetMapping("/test")
+    public ResponseVO test() {
+        return null;
+    }
 }
